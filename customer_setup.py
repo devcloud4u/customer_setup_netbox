c96b1734c60a7bd726427c6af2fde0240e1a234c
Masood windows <masood.kamyab@gmail.com>
2024-05-18 10:51:15 +0330

    Change the tenant slug

--- customer_setup.py
+++ customer_setup.py
@@ -1,3 +1,4 @@
+# script version = 3
 import random
 
 from extras.scripts import Script, StringVar, IntegerVar
@@ -73,9 +74,9 @@ def validate_and_format_subnet_base(ip_base):
     return '.'.join(parts)
 
 
-class CustomerSetupScript(Script):
+class New_Office_Mikrotik_Cloud_Customer_v3(Script):
     class Meta:
-        name = "Customer Setup Script"
+        name = "New Office Mikrotik Cloud Customer v3"
         description = "Sets up tenant, sites, VLANs, and prefixes for a new customer"
         commit_default = True
 
@@ -90,7 +91,10 @@ class CustomerSetupScript(Script):
         try:
             validated_subnet_base = validate_and_format_subnet_base(data['customer_21_subnet'])
             tenant_group, created = TenantGroup.objects.get_or_create(name='Customers')
-            tenant, created = Tenant.objects.get_or_create(name=data['customer_full_name'], group=tenant_group)
+            # Tenant için slug oluştur
+            tenant_slug = slugify(data['customer_full_name'])
+            tenant, created = Tenant.objects.get_or_create(name=data['customer_full_name'], slug=tenant_slug,
+                                                           group=tenant_group)
             self.log_info(f"Tenant {'created' if created else 'retrieved'}: {tenant.name}")
 
             office_site_name = f"{data['customer_short_name']} {data['customer_office_place']}"
@@ -106,14 +110,15 @@ class CustomerSetupScript(Script):
 
             aligned_office_prefix_base = align_to_subnet(validated_subnet_base, 21)
             office_prefix_str = f"{aligned_office_prefix_base}/21"
-            office_prefix, created = Prefix.objects.get_or_create(prefix=office_prefix_str, site=office_site, tenant=tenant, status=PrefixStatusChoices.STATUS_CONTAINER, is_pool=True)
+            office_prefix, created = Prefix.objects.get_or_create(prefix=office_prefix_str, site=office_site,
+                                                                  tenant=tenant,
+                                                                  status=PrefixStatusChoices.STATUS_CONTAINER,
+                                                                  is_pool=True)
             self.log_info(f"/21 prefix for office site {'created' if created else 'retrieved'}: {office_prefix.prefix}")
 
             cloud_vlan_id = int(data['customer_cloud_vlanid'])
             cloud_vlan_name = format_vlan_id(data['customer_cloud_vlanid'])
-            cloud_vlan, created = VLAN.objects.get_or_create(vid=cloud_vlan_id,
-                                                             name=f"{data['customer_short_name']} Cloud",
-                                                             site=cloud_site,
+            cloud_vlan, created = VLAN.objects.get_or_create(vid=cloud_vlan_id, name=cloud_vlan_name, site=cloud_site,
                                                              tenant=tenant)
             self.log_info(f"Cloud VLAN {'created' if created else 'retrieved'}: {cloud_vlan.name}")
 
@@ -133,29 +138,31 @@ class CustomerSetupScript(Script):
             ]
 
             for vid, name, subnet in vlans:
-                vlan, created = VLAN.objects.get_or_create(vid=vid, name=name, site=office_site,tenant=tenant)
+                vlan, created = VLAN.objects.get_or_create(vid=vid, name=name, site=office_site, tenant=tenant)
                 self.log_info(f"{name} VLAN {'created' if created else 'retrieved'}: {vlan.name}")
 
                 prefix_str = f"{subnet}/24"
-                prefix, created = Prefix.objects.get_or_create(prefix=prefix_str, site=office_site, vlan=vlan, tenant=tenant)
+                prefix, created = Prefix.objects.get_or_create(prefix=prefix_str, site=office_site, vlan=vlan,
+                                                               tenant=tenant)
                 self.log_info(f"{name} prefix {'created' if created else 'retrieved'}: {prefix.prefix}")
 
             infra_prefix_str = f"{subnet_addresses[0]}/24"
-            infra_prefix, created = Prefix.objects.get_or_create(prefix=infra_prefix_str, site=office_site, tenant=tenant)
+            infra_prefix, created = Prefix.objects.get_or_create(prefix=infra_prefix_str, site=office_site,
+                                                                 tenant=tenant)
             self.log_info(f"Infra prefix {'created' if created else 'retrieved'}: {infra_prefix.prefix}")
 
             password = generate_password(20)
             save_password_template = '''
-                        
+
             passbolt please save this
             ======
             uri: {data['local_vpn_ip']}:35300
             username: admin
             password: {password}
-            
+
             Office mikrotik code to copy paste
             ======
-            
+
             '''
 
             script_template = f"""
@@ -163,7 +170,7 @@ class CustomerSetupScript(Script):
             :global AdminPassword "{password}"
             :global DnsServers "8.8.8.8,8.8.4.4,1.1.1.1"
 
-            :global CustomerOfficeBigSubnet "{base_ip}.0/21"
+            :global CustomerOfficeBigSubnet "{base_ip}/21"
 
             :global InfraGWIp "{increment_last_octet(subnet_addresses[0], 1)}"
             :global InfraIp "{increment_last_octet(subnet_addresses[0], 1)}/24"
@@ -194,20 +201,20 @@ class CustomerSetupScript(Script):
             :global GuestIpNetwork "{increment_last_octet(subnet_addresses[4], 0)}"
             :global GuestIpSubnet "{increment_last_octet(subnet_addresses[4], 0)}/24"
             :global PoolGuest "{increment_last_octet(subnet_addresses[4], 99)}-{increment_last_octet(subnet_addresses[4], 253)}"
-            
+
             # static variables
             :global OpenVPNProfileName "OpenVPN_Profile"
             :global OpenVPNClientName "OpenVPN_Cloud"
             :global ServiceSubnet "10.200.110.0/24"
             :global OpenVPNServerInterfaceIP "10.200.110.1"
-            
+
             # Cloud Server and Client Office side variables
             :global OpenVPNCloudFirewallIPorHost "95.211.35.162"
             :global OpenVPNCloudUsername "{data['customer_short_name']}-{data['customer_office_place']}-Office"
             :global OpenVPNCloudPassword "{generate_password(20)}"
             :global OpenVPNLocalIP "{data['local_vpn_ip']}"
             :global OpenVPNOfficeSubnets "{aligned_office_prefix_base}/21"
-            
+
             # only Cloud Server Side variables
             :global CustomerAdresList "{cloud_vlan_name}-{data['customer_short_name']}"
             :global CustomerCloudSubnet "10.0.{data['customer_cloud_vlanid']}.0/24"
@@ -221,19 +228,19 @@ class CustomerSetupScript(Script):
             office_site_template = '''
             ### reset to factory
             #/system reset-configuration no-defaults=yes skip-backup=yes
-            
+
             # !!!!! do not use ether1 for programming use last port like port 4 or 5
-            
+
             ### Reset only mac addresses
             #/interface ethernet reset-mac-address
-            
+
             ### template without voipmax
             ### System Name 
             /system identity set name=$NameDevice
-            
+
             ### Set Password
             /user set [find name=admin] password=$AdminPassword
-            
+
             ### Interface Names
             /interface ethernet
             set [ find default-name=ether1 ] name=ether1_Trunk_Switch
@@ -241,17 +248,17 @@ class CustomerSetupScript(Script):
             set [ find default-name=ether3 ] name=ether3_WAN2
             set [ find default-name=ether4 ] name=ether4
             set [ find default-name=ether5 ] name=ether5
-            
+
             ### OpenVPN Clients
             /interface ovpn-client
             # Add OpenVPN client for Ortimo VPN 
             add connect-to=$OpenVPNCloudFirewallIPorHost name=$OpenVPNClientName password=$OpenVPNCloudPassword user=$OpenVPNCloudUsername
-            
-            
+
+
             ### Interface Bridge with VLAN Filtering
             /interface bridge
             add name=Bridge_Trunk vlan-filtering=yes
-            
+
             ### VLAN Interfaces
             # Creating VLAN Interfaces on the Bridge for various network segments
             /interface vlan
@@ -259,7 +266,7 @@ class CustomerSetupScript(Script):
             add interface=Bridge_Trunk vlan-id=30 name="VLAN_0030_Voip"
             add interface=Bridge_Trunk vlan-id=40 name="VLAN_0040_Security"
             add interface=Bridge_Trunk vlan-id=90 name="VLAN_0090_Guest"
-            
+
             ### Bridge VLAN Configuration
             # VLAN 10 untagged on ether1_Trunk_Switch, other VLANs tagged
             /interface bridge vlan
@@ -267,11 +274,11 @@ class CustomerSetupScript(Script):
             add bridge=Bridge_Trunk tagged=ether1_Trunk_Switch,Bridge_Trunk vlan-ids=30
             add bridge=Bridge_Trunk tagged=ether1_Trunk_Switch,Bridge_Trunk vlan-ids=40
             add bridge=Bridge_Trunk tagged=ether1_Trunk_Switch,Bridge_Trunk vlan-ids=90
-            
+
             ### Bridge Ports
             /interface bridge port
             add bridge=Bridge_Trunk interface=ether1_Trunk_Switch
-            
+
             ### Interface Lists
             /interface list
             add name=Infra
@@ -281,10 +288,10 @@ class CustomerSetupScript(Script):
             add name=Guest
             add name=WAN
             add name=Cloud
-            
+
             # Add LAN interface including other interface lists 
             add include=Infra,Office,Security,VoIP,Guest name=LAN
-            
+
             #### IP Pools
             /ip pool
             add ranges=$PoolInfra name=Pool_Infra
@@ -292,7 +299,7 @@ class CustomerSetupScript(Script):
             add ranges=$PoolVoip name=Pool_VoIP
             add ranges=$PoolSecurity name=Pool_Security
             add ranges=$PoolGuest name=Pool_Guest
-            
+
             ### IP Addresses
             /ip address
             # Untagged VLAN 1 used as Infra its bridge
@@ -301,7 +308,7 @@ class CustomerSetupScript(Script):
             add address=$VoipIp interface=VLAN_0030_Voip network=$VoipIpNetwork
             add address=$SecurityIp interface=VLAN_0040_Security network=$SecurityIpNetwork
             add address=$GuestIp interface=VLAN_0090_Guest network=$GuestIpNetwork
-            
+
             ### DHCP Servers
             /ip dhcp-server
             add address-pool=Pool_Infra interface=Bridge_Trunk lease-time=1w name=DHCP_Infra
@@ -309,9 +316,9 @@ class CustomerSetupScript(Script):
             add address-pool=Pool_VoIP interface=VLAN_0030_Voip lease-time=1w name=DHCP_VoIP
             add address-pool=Pool_Security interface=VLAN_0040_Security lease-time=1w name=DHCP_Security
             add address-pool=Pool_Guest interface=VLAN_0090_Guest lease-time=1h name=DHCP_Guest
-            
-            
-            
+
+
+
             ### interface member lists	
             /interface list member
             add interface=ether1_Trunk_Switch list=Infra
@@ -325,16 +332,16 @@ class CustomerSetupScript(Script):
             add interface=ether2_WAN1 list=WAN
             #mybe not needed
             add interface=ether3_WAN2 list=WAN
-            
+
             ### dhcp client 
             /ip dhcp-client
             # main internet
             add add-default-route=yes default-route-distance=1 interface=ether2_WAN1
             # backup internet
             add add-default-route=yes default-route-distance=5 interface=ether3_WAN2
-            
-            
-            
+
+
+
             ### DHCP Servers Network Configuration
             /ip dhcp-server network
             add address=$InfraIpSubnet dns-server=$InfraGWIp gateway=$InfraGWIp
@@ -342,24 +349,24 @@ class CustomerSetupScript(Script):
             add address=$VoipIpSubnet dns-server=$VoipGWIp gateway=$VoipGWIp
             add address=$SecurityIpSubnet dns-server=$SecurityGWIp gateway=$SecurityGWIp
             add address=$GuestIpSubnet dns-server=$GuestGWIp gateway=$GuestGWIp
-            
+
             ### DNS Static Reservations
             /ip dns
             set allow-remote-requests=yes
             /ip dns static
-            
+
             ### routes
             /ip route add disabled=no dst-address=$CustomerCloudSubnet gateway=$OpenVPNServerInterfaceIP routing-table=main suppress-hw-offload=no
-            
-            
+
+
             ### dns server settings
             /ip dns set servers=$DnsServers
             /ip dns set cache-max-ttl=0s
             /ip dns set allow-remote-requests=yes
-            
+
             /ip firewall address-list
             add list=AdminAccess address=$ServiceSubnet
-            
+
             ### IP firewall setup
             /ip firewall filter
             # Allow traffic from OpenVPN_Ortimo VPN to LAN
@@ -395,45 +402,45 @@ class CustomerSetupScript(Script):
             add action=drop chain=forward connection-nat-state=!dstnat out-interface-list=!WAN comment="Drop invalid and non-DSTNAT forward from WAN"
             add action=drop chain=forward connection-nat-state=!dstnat connection-state=new in-interface-list=WAN comment="Drop new non-DSTNAT forward from WAN"
             add action=drop chain=forward connection-state=invalid in-interface-list=WAN comment="Drop invalid forward from WAN"
-            
+
             ### Firewall NAT
             /ip firewall nat
             add action=masquerade chain=srcnat out-interface-list=WAN comment="Masquerade for outbound traffic from LAN to WAN"
-            
+
             ### ip services  disable
             /ip service
             set telnet disabled=yes
             set ftp disabled=yes
             set api disabled=yes
             set api-ssl disabled=yes
-            
+
             ### ip services custom 
             /ip service
             set winbox port=35300
             set www port=8181
             set ssh port=2220
-            
+
             ### system settings 
             /system clock
             set time-zone-name=Europe/Amsterdam
-            
+
             ### time client 
             /system ntp client
             set enabled=yes
             /system ntp client servers
             add address=3.nl.pool.ntp.org
-            
+
             ### sys note
             /system note
             set show-at-login=no
-            
+
             ### sys loggig to memory remember
             /system logging
             /system/logging/action/set memory remember=yes
-            
+
             # add dhcp option to wificontrol (wificontrol unifi inform option)
             /ip dhcp-server option add code=43 name=unifi-wificontrol value=0x010455115F33
-            
+
             # link this option to first dhcp server ! first dhcp server should be infra dhcp server
             # find in ui -ip --> DHCP server --> Networks
             /ip dhcp-server network set 0 dhcp-option=unifi-wificontrol
@@ -442,15 +449,15 @@ class CustomerSetupScript(Script):
             /ip dhcp-server network set 3 dhcp-option=unifi-wificontrol
             /ip dhcp-server network set 4 dhcp-option=unifi-wificontrol
             /ip dhcp-server network set 5 dhcp-option=unifi-wificontrol
-            
+
             #Extra Settings
             /system note set show-at-login=no
             /system logging action set memory remember=yes
-            
+
             #Securing Mikrotik
             /ip ssh set strong-crypto=yes forwarding-enabled=both
-            
-            
+
+
             # extra security 
             /tool mac-server mac-winbox set allowed-interface-list=LAN
             /tool mac-server ping set enabled=no
@@ -461,7 +468,7 @@ class CustomerSetupScript(Script):
             /ip upnp set enabled=no
             /ip cloud set ddns-enabled=no update-time=no
             /lcd set enabled=no
-            
+
             # mikrotik mode change + give options
             /system/device-mode/update mode=enterprise scheduler=yes socks=yes fetch=yes pptp=yes l2tp=yes bandwidth-test=yes traffic-gen=yes sniffer=yes ipsec=yes romon=yes proxy=yes hotspot=yes smb=yes email=yes zerotier=yes container=yes
             # power off on physically in 5 min
@@ -470,19 +477,19 @@ class CustomerSetupScript(Script):
             cloud_site_template = '''
             # Create vlan for customer subnet
             /interface vlan add interface=ether1-Trunk loop-protect=on name=$ClientVLanInterfaceName vlan-id=$ClientVLanID
-            
+
             # interface list
             /interface list add name=$CustomerInterfaceList
             /interface list add include=$CustomerInterfaceList name=Customers
-            
+
             # create openvpn interface
             /interface ovpn-server add name=$OpenVPNServerInterfaceName user=$OpenVPNCloudUsername
             # add openvpn interface to customer list
             /interface list member add interface=$OpenVPNServerInterfaceName list=$CustomerInterfaceList
-            
+
             # add customer interface to customer interface list
             /interface list member add interface=$ClientVLanInterfaceName list=$CustomerInterfaceList
-            
+
             # ip firewall
             # add vpn interface ip to customer
             /ip firewall address-list add address=$OpenVPNLocalIP list=$CustomerAdresList
@@ -490,11 +497,11 @@ class CustomerSetupScript(Script):
             /ip firewall address-list add address=$CustomerOfficeBigSubnet list=$CustomerAdresList
             # add customer cloud subnet to internet access list
             /ip firewall address-list add address=$CustomerCloudSubnet comment=$CustomerCloudSubnetComment list=CloudPC_Internet
-            
+
             # Create openvpn 
             /ppp secret
             add name=$OpenVPNCloudUsername password=$OpenVPNCloudPassword profile=$OpenVPNProfileName remote-address=$OpenVPNLocalIP routes=$OpenVPNOfficeSubnets service=ovpn
-            
+
             # firewall rule 
             /ip firewall filter add action=accept chain=forward comment=$CustomerFirewallRuleComment connection-state=new dst-address-list=$CustomerAdresList in-interface-list=$CustomerInterfaceList out-interface-list=$CustomerInterfaceList src-address-list=$CustomerAdresList place-before=10
             '''
@@ -516,4 +523,4 @@ class CustomerSetupScript(Script):
             self.log_failure(str(e))
 
 
-script = CustomerSetupScript
+script = New_Office_Mikrotik_Cloud_Customer_v3
