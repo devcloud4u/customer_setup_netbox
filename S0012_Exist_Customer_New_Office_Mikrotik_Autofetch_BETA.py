1943cc7273a9b1e2e309e6782a16b638858b38f2
Masood windows <masood.kamyab@gmail.com>
2024-06-25 18:12:20 +0330

    add autofetch for customer 21 subnet

--- S0012_Exist_Customer_New_Office_Mikrotik_Autofetch_BETA.py
+++ S0012_Exist_Customer_New_Office_Mikrotik_Autofetch_BETA.py
@@ -64,7 +64,10 @@ class S0011_Exist_Customer_New_Office_Mikrotik(Script):
     customer_21_subnet = ObjectVar(
         model=Prefix,
         label="Customer 21 Subnet",
-        required=True
+        required=True,
+        query_params={
+            'tag': 'active-customer-office-subnet'
+        }
     )
 
     local_vpn_ip = StringVar(
@@ -84,27 +87,6 @@ class S0011_Exist_Customer_New_Office_Mikrotik(Script):
         label="Customer Address List Name in Cloud Mikrotik",
         required=True
     )
-
-    def prepare(self):
-        # Get the tag and find all prefixes tagged with it
-        tag = Tag.objects.get(slug='active-customer-office-subnet')
-        tagged_prefixes = Prefix.objects.filter(tags__in=[tag])
-        available_subnets = []
-
-        # For each tagged prefix, find available /21 subnets within it
-        for tagged_prefix in tagged_prefixes:
-            prefix_set = netaddr.IPSet([tagged_prefix.prefix])
-            child_prefixes_set = netaddr.IPSet([child.prefix for child in tagged_prefix.get_child_prefixes()])
-            available_prefixes = prefix_set - child_prefixes_set
-
-            for subnet in available_prefixes.iter_cidrs():
-                if subnet.prefixlen == 21:
-                    available_subnets.append(str(subnet))
-
-        # Set the query_params for customer_21_subnet
-        self.customer_21_subnet.field.query_params = {
-            'prefix__in': available_subnets
-        }
 
     @staticmethod
     def validate_and_format_subnet_base(ip_base):
